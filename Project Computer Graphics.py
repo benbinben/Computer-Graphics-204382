623993314a9a11becaffebfa6c9305f79addca62
JUTHAPORN20 <idea_par@hotmail.com>
2020-04-09 03:44:52 +0700

    add cap webcam

--- Project Computer Graphics.py
+++ Project Computer Graphics.py
@@ -8,24 +8,50 @@ from tkinter import *
 from tkinter import filedialog
 import cv2
 
+
+
 def select_image():
-	path = filedialog.askopenfilename()
-	show_image(path)
-	
+        print("You choose select image from your computer")
+        path = filedialog.askopenfilename()
+        show_image(path)
+
+def Cap_webcam():
+        print("You choose capture image from your webcam")
+        cap = cv2.VideoCapture(0)
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            # Display the resulting frame
+            cv2.imshow('frame',frame)
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('c'):
+                    cv2.imwrite('Capture.png', frame)
+                    break
+
+        cap.release()
+        cv2.destroyAllWindows()
+        #img = cv2.imread('Capture.png')
+        #cv2.imshow('Capture show',img)
+        path = 'Capture.png'
+        show_image(path)
+        
 def show_image(path):
+        print(path)
         img = cv2.imread(path)
         cv2.imshow('Show image',img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        
+         
 def main():
         root = Tk()
-        root.geometry('150x50') 
-        btn = Button(root, text="Select an image", command=select_image)
-        btn.pack(side = TOP, pady = 10)
+        root.geometry('150x80') 
+        # Cap image from webcam
+        CapVideo = Button(root, text="Capture webcam", command=Cap_webcam)
+        CapVideo.pack(side = BOTTOM , pady=5)
+        # Select image
+        select = Button(root, text="Select an image", command=select_image)
+        select.pack(side = BOTTOM , pady=10)
         # kick off the GUI
         root.mainloop()
-if __name__ == "__main__":
+if __name__ == "__main__":        
     main()
 
 
